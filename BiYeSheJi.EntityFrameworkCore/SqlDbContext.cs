1be07aaee64c071ecc1abb62a9158004a30f3c00
zhongq111 <2269831144@qq.com>
2025-03-03 20:41:21 +0800

    Update SqlDbContext.cs

--- BiYeSheJi.EntityFrameworkCore/SqlDbContext.cs
+++ BiYeSheJi.EntityFrameworkCore/SqlDbContext.cs
@@ -17,7 +17,7 @@ namespace BiYeSheJi.EntityFrameworkCore
         {
 
         }
-        public DbSet<StuUser> StuUsers { get; set; }
+        public DbSet<User> Users { get; set; }
         public DbSet<Post> Posts { get; set; }
         public DbSet<Comment> Comments { get; set; }
         public DbSet<Reply> Replies { get; set; }
